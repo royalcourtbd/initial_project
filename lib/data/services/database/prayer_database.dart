0a936c32f4307115264b709a4417ba8a00372ae1
royalcourtbd <sayedvai08@gmail.com>
2025-04-08 00:44:07 +0600

    Initial commit

---
+++ lib/data/services/database/prayer_database.dart
@@ -0,0 +1,17 @@
+// import 'package:drift/drift.dart';
+// import 'package:initial_project/data/services/database/database_loader.dart';
+
+// part 'prayer_database.g.dart';
+
+// @DriftDatabase(
+//   tables: [
+//     //
+//   ],
+// )
+// class PrayerDatabase extends _$PrayerDatabase {
+//   PrayerDatabase({QueryExecutor? queryExecutor})
+//     : super(queryExecutor ?? loadDatabase());
+
+//   @override
+//   int get schemaVersion => 1;
+// }
